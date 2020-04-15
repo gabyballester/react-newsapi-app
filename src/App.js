9d576cb4224f8fe60c7ed88a7f364a6b253f7d8b
gabyballester <gaby.ballester@gmail.com>
2020-04-15 12:32:59 +0200

    Implementada la llamada a la api y la recogida de datos y guardado en el state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 
-class App extends Component{
-  state={}
-  render(){
-    return(
+class App extends Component {
+
+  state = {
+    noticias: [] //array vacío para noticias
+  }
+  
+  //Método de creación de componente
+  componentDidMount() { //cuando el componente se monta se ejecuta esto
+   this.consultarNoticias(); //llama a esta función
+  }
+
+  consultarNoticias = async () => { //función asíncrona que llama a la api
+
+   // pegamos aquí el endpoint dentro de la clase con template strings
+   const base_url = `http://newsapi.org/v2/top-headlines?`;
+   const country = `country=us`; //este dato lo sacamos de su web
+   const category = `&category=business`; //este dato lo sacamos de su web
+   const api_key = `&apiKey=94160c6e14b24fcfaba4b1b287ae01cf`; //lo generamos en la web
+   const url = `${base_url}${country}${category}${api_key}`; //construcción url
+
+   //solicitamos la respeuta síncrona con await
+   const respuesta = await fetch(url);
+   const noticias = await respuesta.json(); //paso a json el texto
+
+   this.setState({
+     //metemos los artículos en nuestro array noticias
+     noticias: noticias.articles
+   })
+
+  }
+
+  render() {
+    return (
       <h1>Noticias API React</h1>
     );
   }
